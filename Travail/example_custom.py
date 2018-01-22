40baa94bf4c975d2d5b463109ced96389136a221
Theo Ferrieux <eternal.baladeur@gmail.com>
2018-01-22 19:42:02 +0100

    final lundi 22/01/2018

--- Travail/example_custom.py
+++ Travail/example_custom.py
@@ -20,26 +20,43 @@ class RedStrategy(Strategy):
 		balle=state.ball.position
 		joueur=state.player_state(id_team,id_player).position
 		direction_balle=balle-joueur
-		direction_balle.norm=0.06
-		direction_goal=Vector2D.create_random(-0.5,0.5)
-		if id_team==0:
-			direction_goal=Vector2D(150,45)-balle
-		if id_team==0:
-			direction_goal=Vector2D(0,45)-balle
-		direction_goal.norm=0.5
-		return SoccerAction(direction_balle,direction_goal)
+		direction_balle.norm=0.2
+		distance=math.hypot(balle.x-joueur.x,balle.y-joueur.y)
+		#direction_goal=Vector2D.create_random(-0.5,0.5)
+		#if id_team==0:
+		#	direction_goal=Vector2D(150,45)-balle
+		#if id_team==0:
+		#	direction_goal=Vector2D(0,45)-balle
+		#direction_goal.norm=0.5
+		if distance <= PLAYER_RADIUS+BALL_RADIUS:
+			return SoccerAction(direction_balle,Vector2D(1.0,0.0))
+		else:
+			return SoccerAction(direction_balle)
 
 class BlueStrategy(Strategy):
 	def __init__(self):
 		Strategy.__init__(self,"Blue")
 	def compute_strategy(self,state,id_team,id_player):
-		return SoccerAction(Vector2D(0,0,-3.14/2,0.04),Vector2D(-1.0,0.0))
-
+		balle=state.ball.position
+		joueur=state.player_state(id_team,id_player).position
+		direction_balle=balle-joueur
+		direction_balle.norm=0.2
+		distance=math.hypot(balle.x-joueur.x,balle.y-joueur.y)
+		#direction_goal=Vector2D.create_random(-0.5,0.5)
+		#if id_team==0:
+		#	direction_goal=Vector2D(150,45)-balle
+		#if id_team==0:
+		#	direction_goal=Vector2D(0,45)-balle
+		#direction_goal.norm=0.5
+		if distance <= PLAYER_RADIUS+BALL_RADIUS:
+			return SoccerAction(direction_balle,Vector2D(-1.0,0.0))
+		else:
+			return SoccerAction(direction_balle)
 ## Creation d'une equipe
 red = SoccerTeam(name="Red")
 blue = SoccerTeam(name="Blue")
 red.add("Red One",RedStrategy()) #Strategie qui ne fait rien
-blue.add("Blue One",RedStrategy())   #Strategie aleatoire
+blue.add("Blue One",RandomStrategy())   #Strategie aleatoire
 
 #Creation d'une partie
 simu = Simulation(red,blue)
