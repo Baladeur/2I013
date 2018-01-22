e014b51261ad40084ff005421091c02dade2523d
Theo Ferrieux <eternal.baladeur@gmail.com>
2018-01-22 19:24:30 +0100

    Suivre Balle

--- Travail/example_custom.py
+++ Travail/example_custom.py
@@ -2,6 +2,7 @@ from soccersimulator import Vector2D, SoccerState, SoccerAction
 from soccersimulator import Simulation, SoccerTeam, Player, show_simu
 from soccersimulator import Strategy
 from soccersimulator import settings
+from soccersimulator.settings import *
 import math
 
 
@@ -16,7 +17,17 @@ class RedStrategy(Strategy):
 	def __init__(self):
 		Strategy.__init__(self,"Red")
 	def compute_strategy(self,state,id_team,id_player):
-		return SoccerAction(Vector2D(0.0,0.0,0,0.04),Vector2D(1.0,0.0))
+		balle=state.ball.position
+		joueur=state.player_state(id_team,id_player).position
+		direction_balle=balle-joueur
+		direction_balle.norm=0.06
+		direction_goal=Vector2D.create_random(-0.5,0.5)
+		if id_team==0:
+			direction_goal=Vector2D(150,45)-balle
+		if id_team==0:
+			direction_goal=Vector2D(0,45)-balle
+		direction_goal.norm=0.5
+		return SoccerAction(direction_balle,direction_goal)
 
 class BlueStrategy(Strategy):
 	def __init__(self):
@@ -28,7 +39,7 @@ class BlueStrategy(Strategy):
 red = SoccerTeam(name="Red")
 blue = SoccerTeam(name="Blue")
 red.add("Red One",RedStrategy()) #Strategie qui ne fait rien
-blue.add("Blue One",BlueStrategy())   #Strategie aleatoire
+blue.add("Blue One",RedStrategy())   #Strategie aleatoire
 
 #Creation d'une partie
 simu = Simulation(red,blue)
